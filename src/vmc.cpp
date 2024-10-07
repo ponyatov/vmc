0bb847731ab4f4b71750d69db3da50d1c3769f14
Dmitry Ponyatov <dponyatov@gmail.com>
2024-10-07 21:11:12 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/vmc.cpp
@@ -0,0 +1,12 @@
+#include "vmc.hpp"
+
+int main(int argc, char *argv[]) {  //
+    arg(0, argv[0]);
+    for (int i = 1; i < argc; i++) {  //
+        arg(i, argv[i]);
+    }
+}
+
+void arg(int argc, char *argv) {  //
+    fprintf(stderr, "argv[%i] = <%s>\n", argc, argv);
+}
---
+++ src/vmc.ml
@@ -0,0 +1,76 @@
+type expr =
+  | Int of int
+  | Num of float
+  | Char of char
+  | Str of string
+  | Id of string
+
+let dump (e : expr) : string =
+  match e with
+  | Int n -> "int:" ^ string_of_int n
+  | Num f -> "num:" ^ string_of_float f
+  | Char c -> "char:'" ^ Char.escaped c
+  | Str s -> "str:\"" ^ s ^ "\""
+  | Id id -> "id:" ^ id
+
+(** pretty-printer for utop *)
+let pp (fmt : Format.formatter) (e : expr) = Format.pp_print_string fmt (dump e)
+
+type env = (string * expr) list
+(** environment type (symbol table) *)
+
+(** global environment *)
+let glob : env = [ ("pi", Num 3.1415); ("zero", Int 0) ]
+
+(** lookup symbol in environment *)
+let lookup (env : env) (v : string) : expr = List.assoc v env
+
+(** evaluate expression into  *)
+let eval (env : env) (e : expr) : expr =
+  match e with
+  | Int n -> e
+  | Num f -> e
+  | Char c -> e
+  | Str s -> e
+  | Id v -> lookup env v
+
+let%test "int" = "int:123" = (Int 123 |> eval glob |> dump)
+let%test "id" = "num:3.1415" = (Id "pi" |> eval glob |> dump)
+let%test "zero" = "int:0" = (Id "zero" |> eval glob |> dump)
+
+(** project path: must be in user $HOME *)
+let path = Sys.getenv "HOME" ^ "/vmc"
+
+(** headers code generator *)
+let hpp =
+  let hpp = open_out (path ^ "/inc/vmc.hpp") in
+  Printf.fprintf hpp
+    "#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char *argv[]);
+void arg(int argc, char *argv);
+";
+  close_out hpp
+
+(** app code generator *)
+let cpp =
+  let cpp = open_out (path ^ "/src/vmc.cpp") in
+  Printf.fprintf cpp
+    "#include \"vmc.hpp\"
+
+int main(int argc, char *argv[]) {  //
+    arg(0, argv[0]);
+    for (int i = 1; i < argc; i++) {  //
+        arg(i, argv[i]);
+    }
+}
+
+void arg(int argc, char *argv) {  //
+    fprintf(stderr, \"argv[%%i] = <%%s>\\n\", argc, argv);
+}
+";
+  close_out cpp
